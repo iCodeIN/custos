2031aa04c5f69fd055bdcbeaa804478d93e64deb
Ross Nomann <rossnomann@protonmail.com>
2022-02-05 03:51:26 +0100

    carapax 0.11

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
 use carapax::{
-    handler,
     longpoll::LongPoll,
-    methods::{DeleteMessage, KickChatMember, UnbanChatMember},
-    types::{Integer, Message, MessageData},
-    Api, Config, Dispatcher, ExecuteError,
+    methods::{BanChatMember, DeleteMessage, UnbanChatMember},
+    types::{ChatId, Integer, Message, MessageData, UserId},
+    Api, App, Context, ExecuteError, Ref,
 };
 use dotenv::dotenv;
-use std::env;
+use std::{env, error::Error, fmt};
 
-struct Context {
-    api: Api,
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    env_logger::init();
+    let token = env::var("ONLY_COMMENTS_TOKEN").expect("ONLY_COMMENTS_TOKEN env var is not set or invalid");
+    let api = Api::new(token).expect("Failed to create Api");
+    let mut context = Context::default();
+    context.insert(api.clone());
+    LongPoll::new(api, App::new(context, handler)).run().await;
+}
+
+async fn handler(api: Ref<Api>, message: Message) -> Result<(), AppError> {
+    let chat_id = message.get_chat_id();
+    if let Some(user_ids) = get_new_chat_members(&message) {
+        for user_id in user_ids {
+            api.execute(BanChatMember::new(chat_id, user_id))
+                .await
+                .map_err(|err| AppError::ban(err, chat_id, user_id))?;
+            api.execute(UnbanChatMember::new(chat_id, user_id))
+                .await
+                .map_err(|err| AppError::unban(err, chat_id, user_id))?;
+        }
+        api.execute(DeleteMessage::new(chat_id, message.id))
+            .await
+            .map_err(|err| AppError::delete_message(err, chat_id, message.id))?;
+    } else if let MessageData::LeftChatMember(_) = message.data {
+        api.execute(DeleteMessage::new(chat_id, message.id))
+            .await
+            .map_err(|err| AppError::delete_message(err, chat_id, message.id))?;
+    }
+    Ok(())
 }
 
 fn get_new_chat_members(message: &Message) -> Option<Vec<Integer>> {
@@ -20,50 +48,65 @@ fn get_new_chat_members(message: &Message) -> Option<Vec<Integer>> {
     }
 }
 
-#[handler]
-async fn message_handler(context: &Context, message: Message) -> Result<(), ExecuteError> {
-    let chat_id = message.get_chat_id();
-    if let Some(user_ids) = get_new_chat_members(&message) {
-        for user_id in user_ids {
-            if let Err(err) = context.api.execute(KickChatMember::new(chat_id, user_id)).await {
-                log::error!(
-                    "Failed to kick chat member: {} (chat_id={} user_id={})",
-                    err,
-                    chat_id,
-                    user_id
-                )
-            }
-            if let Err(err) = context.api.execute(UnbanChatMember::new(chat_id, user_id)).await {
-                log::error!(
-                    "Failed to unban chat member: {} (chat_id={} user_id={})",
-                    err,
-                    chat_id,
-                    user_id
-                )
-            }
-        }
-        if let Err(err) = context.api.execute(DeleteMessage::new(chat_id, message.id)).await {
-            log::error!(
-                "Failed to delete message: {} (chat_id={} message_id={})",
-                err,
-                chat_id,
-                message.id
-            )
-        }
-    } else if let MessageData::LeftChatMember(_) = message.data {
-        context.api.execute(DeleteMessage::new(chat_id, message.id)).await?;
-    }
-    Ok(())
+#[derive(Debug)]
+enum AppError {
+    Ban(ExecuteError, ChatId, UserId),
+    Unban(ExecuteError, ChatId, UserId),
+    DeleteMessage(ExecuteError, ChatId, Integer),
 }
 
-#[tokio::main]
-async fn main() {
-    dotenv().ok();
-    env_logger::init();
-    let token = env::var("ONLY_COMMENTS_TOKEN").expect("ONLY_COMMENTS_TOKEN env var is not set or invalid");
-    let config = Config::new(token);
-    let api = Api::new(config).unwrap();
-    let mut dispatcher = Dispatcher::new(Context { api: api.clone() });
-    dispatcher.add_handler(message_handler);
-    LongPoll::new(api, dispatcher).run().await;
+impl AppError {
+    fn ban<C, U>(source: ExecuteError, chat_id: C, user_id: U) -> Self
+    where
+        C: Into<ChatId>,
+        U: Into<UserId>,
+    {
+        Self::Ban(source, chat_id.into(), user_id.into())
+    }
+
+    fn unban<C, U>(source: ExecuteError, chat_id: C, user_id: U) -> Self
+    where
+        C: Into<ChatId>,
+        U: Into<UserId>,
+    {
+        Self::Unban(source, chat_id.into(), user_id.into())
+    }
+
+    fn delete_message<C>(source: ExecuteError, chat_id: C, message_id: Integer) -> Self
+    where
+        C: Into<ChatId>,
+    {
+        Self::DeleteMessage(source, chat_id.into(), message_id)
+    }
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        use self::AppError::*;
+        match self {
+            Ban(source, chat_id, user_id) => write!(
+                out,
+                "failed to ban chat member: {source} (chat_id={chat_id}, user_id={user_id})"
+            ),
+            Unban(source, chat_id, user_id) => write!(
+                out,
+                "failed to unban chat member: {source} (chat_id={chat_id}, user_id={user_id})"
+            ),
+            DeleteMessage(source, chat_id, message_id) => write!(
+                out,
+                "failed to delete message: {source} (chat_id={chat_id}, message_id={message_id})"
+            ),
+        }
+    }
+}
+
+impl Error for AppError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        use self::AppError::*;
+        Some(match self {
+            Ban(source, _, _) => source,
+            Unban(source, _, _) => source,
+            DeleteMessage(source, _, _) => source,
+        })
+    }
 }
